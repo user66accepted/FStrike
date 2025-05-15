3d8633f5775ec153516ef5c8207aa796f6bfa996
user66accepted <user66accepted@gmail.com>
2025-05-15 14:17:37 +0500

    tracking url update

--- FStrikeBackend/config.js
+++ FStrikeBackend/config.js
@@ -5,7 +5,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || 'YourSuperSecretKey123!@#',
   
   // Tracking configuration - force HTTP
-  trackingUrl: 'http://147.93.87.182:5000',
+  trackingUrl: 'https://railtel.ddns.net',
   
   // Ngrok configuration
   useNgrok: false, // Disable ngrok since we're using VPS
