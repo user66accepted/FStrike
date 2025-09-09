80e3428bd251a0ce91386e94fb92b7da63331b89
mfarrukh14 <87035938+mfarrukh14@users.noreply.github.com>
2025-09-09 10:52:42 +0500

    domain name changed

--- FStrikeBackend/config.js
+++ FStrikeBackend/config.js
@@ -5,7 +5,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || 'YourSuperSecretKey123!@#',
   
   // Tracking configuration - force HTTP
-  trackingUrl: 'https://googlesecurity.ddns.net',
+  trackingUrl: 'https://spaceform.ddns.net',
   
   // Ngrok configuration
   useNgrok: false, // Disable ngrok since we're using VPS
